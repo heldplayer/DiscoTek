0748db7a4515b9c703b83f12ca86f4d9c85674cf
mbl111 <matt@mbl111.com>
2013-08-02 11:56:40 +1000

    Yep... Thats all I have done D:

---
+++ src/net/specialattack/modjam/BleighMod.java
@@ -0,0 +1,43 @@
+package net.specialattack.modjam;
+
+import java.io.File;
+
+import net.minecraftforge.common.Configuration;
+
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Constants.MOD_ID , name = Constants.MOD_NAME, version = Constants.MOD_VERSION)
+
+public class BleighMod{
+
+	@Instance("BleighMod")
+	public BleighMod instance;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event){
+		File configFile = event.getSuggestedConfigurationFile();
+		
+		Configuration config = new Configuration(configFile);
+		config.load();
+			
+		config.save();
+	}
+	
+	@EventHandler
+	public void load(FMLInitializationEvent event){
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event){
+		
+		
+	}
+	
+}
---
+++ src/net/specialattack/modjam/Constants.java
@@ -0,0 +1,8 @@
+package net.specialattack.modjam;
+
+public class Constants {
+
+	public static final String MOD_ID = "BleighMod";
+	public static final String MOD_NAME = "Bleigh Mod";
+	public static final String MOD_VERSION = "Pre-Release";
+}
