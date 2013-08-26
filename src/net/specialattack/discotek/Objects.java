9516eea78b59e79027b302531b5e9edc86fcf180
Heldplayer <mitchel.pyl@gmail.com>
2013-08-26 20:56:13 +0200

    Do some conversions

--- src/net/specialattack/discotek/Objects.java
+++ src/net/specialattack/discotek/Objects.java
@@ -1,6 +1,9 @@
 
 package net.specialattack.discotek;
 
+import java.util.logging.Logger;
+
+import me.heldplayer.util.HeldCore.ModInfo;
 import net.specialattack.discotek.block.BlockColoredLamp;
 import net.specialattack.discotek.block.BlockController;
 import net.specialattack.discotek.block.BlockDecoration;
@@ -15,12 +18,25 @@ import net.specialattack.discotek.item.ItemWirelessLinker;
 
 public class Objects {
 
+    public static final String MOD_ID = "discotek";
+    public static final String MOD_NAME = "DiscoTek";
+    public static final String MOD_VERSION = "@VERSION@";
+    public static final String MOD_CHANNEL = "ModjamMod2013";
+
+    public static final String COMMON_PROXY = "net.specialattack.discotek.CommonProxy";
+    public static final String CLIENT_PROXY = "net.specialattack.discotek.client.ClientProxy";
+
+    public static final ModInfo MOD_INFO = new ModInfo(MOD_ID, MOD_NAME, MOD_VERSION);
+
+    public static Logger log;
+
     public static BlockLight blockLight;
     public static BlockTruss blockTruss;
     public static BlockDecoration blockDecoration;
     public static BlockController blockController;
     public static BlockColoredLamp blockColoredLampOn;
     public static BlockColoredLamp blockColoredLampOff;
+
     public static ItemDebug itemDebug;
     public static ItemLens itemLens;
     public static ItemWirelessLinker itemWirelessLinker;
